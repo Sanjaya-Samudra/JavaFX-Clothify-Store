920156c9ff2e5cb74aa709750df56644ff00c253
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-10-10 23:17:32 +0530

    Initial Commit - Project ER and UseCase

---
+++ ecom/src/main/java/project/Starter.java
@@ -0,0 +1,18 @@
+package project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/dash_form.fxml"))));
+        stage.show();
+
+    }
+}
