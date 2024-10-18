8c4abd7998f0e5690254dab9ba0c7cea65fce587
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2024-10-18 20:14:13 +0530

    Product Management and Custom CSS Added

--- ecom/src/main/java/dto/Item.java
+++ ecom/src/main/java/dto/Item.java
@@ -1,4 +1,12 @@
 package dto;
 
 public class Item {
+    private String id;
+    private String name;
+    private String size;
+    private Double price;
+    private Integer quantity;
+    private String category;
+    private Integer stock;
+    private String supplierName;
 }
